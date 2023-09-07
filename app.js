e10c756810ce3a56ee1fdfed68184af79f463050
gauravkumarchhipa <gauravchhipa295@gmail.com>
2023-09-07 17:37:01 +0530

    cors error in local

--- app.js
+++ app.js
@@ -13,11 +13,13 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(
-    cors({
-        origin: process.env.FRONTEND_URL,
-        credentials: true,
-        methods: ["GET", "POST", "PUT", "DELETE"],
-    })
+    cors(
+        // {
+        //     origin: process.env.FRONTEND_URL,
+        //     credentials: true,
+        //     methods: ["GET", "POST", "PUT", "DELETE"],
+        // }
+    )
 );
 app.use(
     express.urlencoded({
