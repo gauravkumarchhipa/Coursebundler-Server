d36c6a8b87918104c033cd5e50ca0729e6c8057a
gaurav.chhipa <gaurav.c@zyapaar.com>
2023-11-23 15:15:57 +0530

    cross error multiple allow

--- app.js
+++ app.js
@@ -13,10 +13,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin:
-      process.env.NODE_ENV === "DEVELOPMENT"
-        ? process.env.FRONTEND_URL_LOCAL
-        : process.env.FRONTEND_URL,
+    origin: [process.env.FRONTEND_URL_LOCAL, process.env.FRONTEND_URL],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
